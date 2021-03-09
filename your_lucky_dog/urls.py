88e70d5d3673a99b6f513eab7fac5056e3f92d38
Tess2019 <terese_burman@hotmail.com>
2021-03-09 09:23:19 +0000

    added user and shopping basket

--- your_lucky_dog/urls.py
+++ your_lucky_dog/urls.py
@@ -22,4 +22,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('', include('home.urls')),
+    path('products/', include('products.urls')),
+    path('basket/', include('baskets.urls')),
+    path('profile/', include('profiles.urls')),
+    path('checkout/', include('checkout.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
