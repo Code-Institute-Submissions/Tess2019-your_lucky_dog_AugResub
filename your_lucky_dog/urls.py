032dfb2a193987d63dcf9b1bdc6319a5330b1850
Tess2019 <terese_burman@hotmail.com>
2021-03-16 10:10:34 +0000

    changed place for the includes in base.html

--- your_lucky_dog/urls.py
+++ your_lucky_dog/urls.py
@@ -24,6 +24,6 @@ urlpatterns = [
     path('', include('home.urls')),
     #path('products/', include('products.urls')),
     #path('basket/', include('baskets.urls')),
-   # path('profile/', include('profiles.urls')),
+    #path('profile/', include('profiles.urls')),
     #path('checkout/', include('checkout.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
