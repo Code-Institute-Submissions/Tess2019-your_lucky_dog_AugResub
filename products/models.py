408599f8eb52be32ffd280728781748f8305e8c5
Tess2019 <terese_burman@hotmail.com>
2021-04-09 17:58:25 +0000

    costumized admin

--- products/models.py
+++ products/models.py
@@ -2,14 +2,18 @@ from django.db import models
 
 
 class Categories(models.Model):
-    name = models.CharField(max_length=254)
-    friendly_name = models.CharField(max_length=254, null=True, blank=True)
 
-    def _str_(self):
-        return self.name
+        class Meta:
+            verbose_name_plural = 'Categories'
 
-    def _get_friendly_name(self):
-        return self.friendly_name
+        name = models.CharField(max_length=254)
+        friendly_name = models.CharField(max_length=254, null=True, blank=True)
+
+        def _str_(self):
+            return self.name
+
+        def _get_friendly_name(self):
+            return self.friendly_name
 
 
 class Product(models.Model):
