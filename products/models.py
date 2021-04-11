0ce0b7a491905dfb050be2e5af5f7cad72bd862f
Tess2019 <terese_burman@hotmail.com>
2021-04-11 19:21:18 +0000

    added sorting

--- products/models.py
+++ products/models.py
@@ -3,27 +3,28 @@ from django.db import models
 
 class Category(models.Model):
 
-        class Meta:
-            verbose_name_plural = 'Categories'
+    class Meta:
+        verbose_name_plural = 'Categories'
 
-        name = models.CharField(max_length=254)
-        friendly_name = models.CharField(max_length=254, null=True, blank=True)
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
 
-        def _str_(self):
-            return self.name
+    def _str_(self):
+        return self.name
 
-        def _get_friendly_name(self):
-            return self.friendly_name
+    def _get_friendly_name(self):
+        return self.friendly_name
 
 
 class Product(models.Model):
-    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
-    # copied from ckz8780
+    category = models.ForeignKey('Category', null=True, blank=True,
+    on_delete=models.SET_NULL)
     sku = models.CharField(max_length=254, null=True, blank=True)
     name = models.CharField(max_length=254)
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
-    rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
+    rating = models.DecimalField(max_digits=6, decimal_places=2, null=True,
+    blank=True)
     designer = models.CharField(max_length=254, null=True, blank=True)
     images_url = models.URLField(max_length=1024, null=True, blank=True)
     images = models.ImageField(null=True, blank=True)
