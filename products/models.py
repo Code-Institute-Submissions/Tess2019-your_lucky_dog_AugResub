f4435563f10323109935ea72cdb64b99b6a9183e
Tess2019 <terese_burman@hotmail.com>
2021-04-14 20:47:00 +0000

    added back to top link and sorting js

--- products/models.py
+++ products/models.py
@@ -23,7 +23,7 @@ class Product(models.Model):
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
     rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
-    designer = models.TextField()
+    designer = models.CharField(max_length=254, null=True, blank=True)
     images_url = models.URLField(max_length=1024, null=True, blank=True)
     images = models.ImageField(null=True, blank=True)
 
