1da99b9008d2d923b8a44dc2d785241534902844
Tess2019 <terese_burman@hotmail.com>
2021-04-19 14:43:22 +0000

    deleted and created new database

--- products/admin.py
+++ products/admin.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
-from .models import Product, Categories
+from .models import Product, Category
 
 # Register your models here.
 
 
 class ProductAdmin(admin.ModelAdmin):
-    list_display = (
+    list_display = ([
         'sku',
         'name',
         'category',
@@ -14,17 +14,17 @@ class ProductAdmin(admin.ModelAdmin):
         'designer',
         'images',
 
-    )
+    ])
 
     ordering = ('sku',)
 
 
-class CategoriesAdmin(admin.ModelAdmin):
-    list_display = (
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ([
         'friendly_name',
         'name',
-    )
+    ])
 
 
 admin.site.register(Product, ProductAdmin)
-admin.site.register(Categories, CategoriesAdmin)
+admin.site.register(Category, CategoryAdmin)
--- products/models.py
+++ products/models.py
@@ -1,15 +1,14 @@
 from django.db import models
 
 
-class category(models.Model):
+class Category(models.Model):
 
     class Meta:
         verbose_name_plural = 'Categories'
 
     name = models.CharField(max_length=254)
     friendly_name = models.CharField(max_length=254, null=True, blank=True)
-
-    products = models.ManyToManyField('Product')
+    products = models.ManyToManyField('Product', related_name='categories')
 
     def __str__(self):
         return self.name
@@ -19,7 +18,7 @@ class category(models.Model):
 
 
 class Product(models.Model):
-    category = models.ManyToManyField('Category')
+    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
     sku = models.CharField(max_length=254, null=True, blank=True)
     name = models.CharField(max_length=254)
     description = models.TextField()
