7de22e31ea69d1d190f5f721e8d3fc4b52d51d1b
Tess2019 <terese_burman@hotmail.com>
2021-04-21 07:28:05 +0000

    added dropdown options in admin

--- products/views.py
+++ products/views.py
@@ -20,7 +20,7 @@ def all_products(request):
             if sortkey == 'name':
                 sortkey = 'lower_name'
                 products = products.annotate(lower_name=Lower('name'))
-            if sortkey == category:
+            if sortkey == categories:
                 sortkey = 'category__name'
             if 'direction' in request.GET:
                 direction = request.GET['direction']
@@ -63,4 +63,4 @@ def product_detail(request, product_id):
         'product': product,
     }
 
-    return render(request, 'products/product_detail.html', context)
+    return render(request, 'product/product_detail.html', context)
