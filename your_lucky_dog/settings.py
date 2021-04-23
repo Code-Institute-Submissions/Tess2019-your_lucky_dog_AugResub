67e83879c325a6139dcc05d639bd7730245314a3
Tess2019 <terese_burman@hotmail.com>
2021-04-23 15:07:43 +0000

    deployment 1.2

--- your_lucky_dog/settings.py
+++ your_lucky_dog/settings.py
@@ -114,10 +114,11 @@ if 'DATABASE_URL' in os.environ:
 else:
     DATABASES = {
         'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
+           'ENGINE': 'django.db.backends.sqlite3',
+           'NAME': BASE_DIR / 'db.sqlite3',
         }
     }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
