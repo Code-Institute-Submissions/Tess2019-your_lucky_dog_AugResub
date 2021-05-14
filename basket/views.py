83bd3d601b738eb0ab6d28fb7d2ed70fc122ce77
Tess2019 <terese_burman@hotmail.com>
2021-05-14 08:51:58 +0000

    added adjust view.py

--- basket/views.py
+++ basket/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render, redirect, reverse, HttpResponse
+from django.shortcuts import render, redirect, reverse, HttpResponse, get_object_or_404
 from django.contrib import messages
-from products.models import Product   
+from products.models import Product
 
 # Create your views here.
 
@@ -14,16 +14,36 @@ def view_basket(request):
 def add_to_basket(request, item_id):
     """ add a quantity of specified products to shopping basket """
 
+    product = get_object_or_404(Product, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
-
-    """ add the basket varible if exist in session or create it """
     basket = request.session.get('basket', {})
 
     if item_id in list(basket.keys()):
         basket[item_id] += quantity
+        messages.success(request, f'Updated {product.name} quantity to {basket[item_id]}')
     else:
         basket[item_id] = basket.get(item_id, 0) + quantity
+        messages.success(request,f'Added {product.name} to your basket')
 
     request.session['basket'] = basket
     return redirect(redirect_url)
+
+
+def adjust_basket(request, item_id):
+    """ adjust the quantity of specified products to the specified amount """
+ 
+    product = get_object_or_404(Product, pk=item_id)
+    quantity = int(request.POST.get('quantity'))
+    quantity = int(request.POST.get('quantity'))
+    basket = request.session.get('basket', {})
+
+    if quantity > 0:
+        basket[item_id] = quantity
+        messages.success(request, f'Updated {product.name} quantity to {basket[item_id]}')
+    else:
+        basket.pop(item_id)
+        messages.success(request,f'Removed {product.name} to your basket')
+
+    request.session['basket'] = basket
+    return redirect(reverse('view_basket')
