70ae4e21e56b8d99e4b42fc2da2a1033f40b7d10
Tess2019 <terese_burman@hotmail.com>
2021-05-20 19:04:25 +0000

    added stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -13,6 +13,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = { 
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51ItF0wCgSnLukhN2UgqzkPQgvAa0KwvFkHZxRh93APvEYT4zGkKINQftUcXhzT1mq9mCFPtefXL2XEzQOZpUWkuI00wa47R8DN',
+        'client_secret':'test client secret',
         }
 
     return render(request, template, context) 
